2653fc6c638db668391a54a9f8c6bbed2645f78e
Matthew Watkins <mwatkins@linuxfoundation.org>
2025-01-29 16:25:14 +0000

    Feat: Enhance unused actions script to not count lfit/releng-reusable-workflows
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- .github/unused-actions.sh
+++ .github/unused-actions.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2024 The Linux Foundation <matthew.watkins@linuxfoundation.org>
 
-# Script to check if actions are referenced in workflows
+# Checks whether GitHub actions are referenced in this repository
 
 set -eu -o pipefail
 
@@ -11,7 +11,7 @@ GITHUB_ACTIONS=$(ls actions)
 
 for ACTION in $GITHUB_ACTIONS; do
     # Recursive grep to check for references
-    if ! (grep -rq "$ACTION"); then
+    if ! (grep -r "$ACTION" | grep -vq 'lfit/releng-reusable-workflows'); then
         echo "Not referenced: $ACTION"
     fi
 done
